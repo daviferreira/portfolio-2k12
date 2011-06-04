13d1dc671fee3ab4423d5a31f1190f3847c61dd7
daviferreira <contato@daviferreira.com>
2011-06-04 09:16:21 -0300

    jQuery.

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module Portfolio
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    # jQuery
+    config.action_view.javascript_expansions[:defaults] = ['jquery', 'rails']
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110604001940) do
+ActiveRecord::Schema.define(:version => 20110604004256) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20110604001940) do
     t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ip"
   end
 
   create_table "post_categories", :force => true do |t|
@@ -48,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20110604001940) do
     t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "tags"
   end
 
   create_table "projects", :force => true do |t|
@@ -60,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20110604001940) do
     t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "tags"
   end
 
   create_table "tags", :force => true do |t|
