8bec33bbf1bda5cc640383960c200b7aa9edf5de
Davi Ferreira <contato@daviferreira.com>
2011-06-27 09:24:50 -0300

    Trying new akismet key.

--- config/initializers/akismet.rb
+++ config/initializers/akismet.rb
@@ -1,3 +1,3 @@
 # -*- encoding : utf-8 -*-
-Portfolio::Application.config.rakismet.key = 'beb8a7e05f40'
+Portfolio::Application.config.rakismet.key = 'c239d1fafb0d'
 Portfolio::Application.config.rakismet.url = 'http://www.daviferreira.com'
