cfc6886ab3ddd1b43baa5c2bb575b4103a3087ef
Davi Ferreira <contato@daviferreira.com>
2011-09-08 12:57:01 -0300

    Sanitizing preserving code block.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -5,4 +5,13 @@ module CommentsHelper
     gravatar_image_tag(comment.email.downcase, :alt => comment.author, :class => 'gravatar', :gravatar => options)
   end
 
+  def sanitize_preserving_code(comment)
+    require 'nokogiri'
+    doc = Nokogiri::HTML::DocumentFragment.parse comment
+    doc.css('code').each do |code|
+      code.inner_html = CGI.escapeHTML code.inner_html.gsub('<br>', '')
+    end
+    sanitize doc.to_html, :tags => %w(strong em code br a) 
+  end
+
 end
