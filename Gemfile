c888a0c5bab3241f108248057a02282659efece2
Davi Ferreira <contato@daviferreira.com>
2011-10-21 21:11:56 -0200

    Fixing posts links at home page, tests.

--- Gemfile
+++ Gemfile
@@ -23,8 +23,9 @@ end
 group :test do
   gem 'rspec'
   gem 'webrat'
-  gem 'spork'
+  gem 'spork', :git => "git://github.com/chrismdp/spork.git"
   gem 'ZenTest'
 	gem 'factory_girl_rails'
   gem 'simplecov', :require => false
+  gem 'ruby-debug19', :require => 'ruby-debug'  
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,4 +3,40 @@ require 'spec_helper'
 
 describe CommentsController do
 
+  describe "POST 'create'" do
+    before(:each) do
+      @post = Factory(:post)
+      @comment = { :body => "Great post!",
+                   :url => "http://www.daviferreira.com",
+                   :email => "contato@daviferreira.com",
+                   :author => "Davi Ferreira",
+                   :post_id => @post.id }
+      class ActionController::Request
+        def remote_ip
+          '192.178.33.4'
+        end
+      end
+    end
+
+    it "should accept a valid comment" do
+      lambda do
+        post :create, :comment => @comment, :format => :js
+      end.should change(Comment, :count).by(1)
+    end
+
+    it "should block spams" do
+      post :create, :comment => @comment.merge({:body => "Viagra"}), 
+                    :format => :js
+      Comment.last.published.should be_false
+    end
+
+    it "should block spams" do
+      post :create, :comment => @comment, 
+                    :format => :js
+      debugger
+      Comment.last.published.should be_true
+    end
+
+  end
+
 end
