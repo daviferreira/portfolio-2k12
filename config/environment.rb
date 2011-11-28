2761a80fb5392d1710bd57ee9f0da47e0ca84a62
Davi Ferreira <davi.ferreira@corp.globo.com>
2011-11-28 17:12:00 -0200

    Removendo timestamp dos assets.

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV['RAILS_ASSET_ID'] = ''
+
 # Initialize the rails application
 Portfolio::Application.initialize!
