16e5d4cab9bd1c13176afe3a1ddd57882bf4da75
Davi Ferreira <contato@daviferreira.com>
2011-12-24 10:39:58 -0200

    Fixing test database issues.

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -10,8 +10,9 @@ describe PostsController do
     
     before(:each) do
       @post = Factory(:post)
-      tableless_post = Factory(:post, :tableless => 'http://www.tableless.com.br/')
-      unpublished_post = Factory(:post, :published => false)
+      tableless_post = Factory(:post, :title => 'Tableless post', :tableless => 'http://www.tableless.com.br/')
+      unpublished_post = Factory(:post, :title => 'Not published', :published => false)
+      blocked_comments_post = Factory(:post, :title => 'Blocked comments', :block_comments => true)
     end
     
     it "should be successful" do
@@ -21,8 +22,12 @@ describe PostsController do
     
     it "should list only published posts" do
       get :index
-      Post.published.all.each do |post|
-        response.should have_selector('a', :content => post.title)
+      Post.all.each do |post|
+        if post.published?
+          response.should have_selector('a', :content => post.title)
+        else
+          response.should_not have_selector('a', :content => post.title)
+        end
       end
     end
     
@@ -45,6 +50,18 @@ describe PostsController do
     it "should not display posts comments from posts with blocked comments" do
       get :index
     end
+    
+  end
+  
+  describe "GET 'show'" do
+    
+  end
+  
+  describe "GET 'feed'" do
+    
+  end
+  
+  describe "GET 'feed_comments'" do
     
   end
 
